7103c5497fd64d9cab2251036fd5ada72c28b25c
anitera <atira95@gmail.com>
2017-11-05 23:38:15 +0200

    edit nickname and connection to server (5.3 completed)

--- client.py
+++ client.py
@@ -1,13 +1,23 @@
 from socket import AF_INET, SOCK_STREAM, socket
 from argparse import ArgumentParser
-
+import curses
+import os
 buffer_length = 1024
 
+nicknames = []
+
+def validate_nickname(nickname):
+    if len(nickname) == 0 or ' ' in nickname or len(nickname) > 8:
+        return False
+    else:
+        return True
+
 if __name__ == '__main__':
+    '''
     parser = ArgumentParser()
     parser.add_argument('-n','--nickname',\
                         help='nickname validation',\
-                        required=True
+                        required=False
                         )
     parser.add_argument('-p','--port',\
                         help='port',\
@@ -19,6 +29,54 @@ if __name__ == '__main__':
                         required=False,\
                         default='127.0.0.1')
     args = parser.parse_args()
+    '''
+
+    if os.path.exists("client.ini"):
+        with open("client.ini", "r+") as cli:
+            for id, nick in enumerate(cli.readlines()):
+                nicknames.append(nick)
+                print id, nick
+    nick = ""
+    if len(nicknames) > 0:
+        while True:
+            id = raw_input("Choose id: ")
+            id = int(id)
+            if id >= 0 and id < len(nicknames):
+                nick = nicknames[id]
+                break
+    else:
+        while True:
+            nick = raw_input("Enter nickname: ")
+            if validate_nickname(nick):
+                if nick not in nicknames:
+                    nicknames.append(nick)
+                else:
+                    print "already exist!"
+                break
+                
+
+        with open("client.ini", "w") as cli:
+            for nick in nicknames:
+                cli.write(nick+'\n')
+
+    print "Your nickname: ", nick
+    
+    s = socket(AF_INET, SOCK_STREAM)
+
+    while True:
+        inpt = raw_input("Enter dedicated host address [ip:port]: ")
+        host, port = inpt.split(":")
+        port = int(port)
+
+        if s.connect_ex( (host, port) ) != 0:
+            print "Incorrect host address. Try again!"
+
+        else:
+            print "Connection established!"
+            break
+    
+    print "Multiplayer Game"
+    '''
     s = socket(AF_INET, SOCK_STREAM)
 
     server_address = (args.Host, int(args.port))
@@ -33,4 +91,8 @@ if __name__ == '__main__':
     except socket.error:
         print 'Socket error'
     print message
+
+    if message == "1":
+        print ""
+    '''
     s.close()
