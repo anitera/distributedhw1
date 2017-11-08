96ede9f7491377b5e22cbd37df5c34eaedfb560d
Viacheslav Komisarenko <komisarenko.slava@yandex.ru>
2017-11-08 19:48:24 +0200

    Update Board_gui.py 2

--- Board_gui.py
+++ Board_gui.py
@@ -1,10 +1,16 @@
-import tkinter
+try:
+    import tkinter as tk
+except ImportError:
+    import Tkinter as tk
 import numpy as np
 
 class Board():
     def __init__(self):
         self.board = tkinter.Tk()
-        w = tkinter.Canvas(self.board, width=630, height=630)
+        self.cell_size = 60
+        self.board_width = 15 * self.cell_size
+        self.board_height = 10 * self.cell_size
+        w = tkinter.Canvas(self.board, width=self.board_width, height=self.board_height)
         self.canvas = w
         self.board_matrix = [[1 for k in range(9)] for k in range(9)]
         self.numbers_dict = {1 : 'blue', 2: 'green', 3: 'blue', 4: 'red', 5: 'black',
@@ -14,15 +20,21 @@ class Board():
     def initialize_board(self):
         for i in range(9):
             for j in range(9):
-                self.canvas.create_rectangle(70 * i, 70 * j, 70 * (i + 1), 70 * (j + 1))
+                self.canvas.create_rectangle(self.cell_size * i, self.cell_size * j,
+                                             self.cell_size * (i + 1), self.cell_size * (j + 1))
                 if i % 3 == 0:
-                    self.canvas.create_line(0, i * 70, 9 * 70, i * 70, width=3)
+                    self.canvas.create_line(0, i * self.cell_size, 9 * self.cell_size,
+                                            i * self.cell_size, width=3)
                 if j % 3 == 0:
-                    self.canvas.create_line(j * 70, 0, j * 70, 9 * 70, width=3)
+                    self.canvas.create_line(j * self.cell_size, 0, j * self.cell_size,
+                                            9 * self.cell_size, width=3)
         self.canvas.pack()
         
     def show_board(self):
         self.board.mainloop()
+    
+    def set_board_number(self, i, j, value):
+        self.board_matrix[i][j] = value
     
     def set_board_numbers(self, matrix):
         self.board_matrix = matrix
@@ -30,7 +42,8 @@ class Board():
     def draw_board_numbers(self):
         for i in range(len(self.board_matrix)):
             for j in range(len(self.board_matrix[0])):
-                self.canvas.create_text(i * 70 + 35, j * 70 + 35, activefill = 'olive',
+                self.canvas.create_text(i * self.cell_size + self.cell_size / 2,
+                                        j * self.cell_size + self.cell_size / 2, activefill = 'olive',
                                         fill = self.numbers_dict[int(self.board_matrix[i][j])],
                               font="Times 20 italic bold",
                               text=self.board_matrix[i][j])
@@ -40,5 +53,6 @@ for i in range(9):
 board = Board()
 board.initialize_board()
 board.set_board_numbers(matrix)
+board.set_board_number(3, 4, 8)
 board.draw_board_numbers()
 board.show_board()
